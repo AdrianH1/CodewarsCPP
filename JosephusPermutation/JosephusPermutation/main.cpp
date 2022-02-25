0015e687590b89ed5bc5fb258ed330b688445f36
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2022-02-25 10:39:33 +0100

    Josephus Permutation - Kata https://www.codewars.com/kata/5550d638a99ddb113e0000a2/solutions/cpp

---
+++ JosephusPermutation/JosephusPermutation/main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+std::vector < int> josephus(std::vector < int > items, int k) {
+    std::vector<int> result;
+    for (int i = k; items.size() > 0; i += (k-1))
+    {
+        while (i > items.size())
+            i = i - items.size();
+        result.push_back(items.at(i-1));
+        items.erase(items.begin() + (i - 1));
+
+    }
+    return result;
+}
+
+
+int main()
+{
+    josephus({ 1, 2, 3, 4, 5, 6, 7 }, 3);//, Equals(std::vector < int >({ 3,6,2,7,5,1,4 })));
+    josephus({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1);//, Equals(std::vector < int >({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })));
+    josephus({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 2);//, Equals(std::vector < int >({ 2, 4, 6, 8, 10, 3, 7, 1, 9, 5 })));
+    josephus({ 1, 2, 3, 4, 5, 6, 7 }, 3);//, Equals(std::vector < int >({ 3, 6, 2, 7, 5, 1, 4 })));
+    josephus({}, 3);//, Equals(std::vector < int >({})));
+}
